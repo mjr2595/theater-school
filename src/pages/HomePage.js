617c1baef2577d33dad139eb89c68e241e86cff6
mjr2595 <michaeljohnraymond@gmail.com>
2023-01-17 23:25:12 -0600

    adding stuff to homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,2 +1,26 @@
-const HomePage = () => {};
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
+import MainSplashImage from "../app/assets/img/main-splash.jpg";
+
+const HomePage = () => {
+    return (
+        <Container className="text-center">
+            <Row>
+                <h2>
+                    Lights, Camera, <i>Reaction</i>
+                </h2>
+            </Row>
+            <Row>
+                <a role="button" className="btn btn-dark btn-lg" href="/classes">
+                    Sign up for a class now!
+                </a>
+            </Row>
+            <Row className="d-flex justify-content-center">
+                <Col>
+                    <img src={MainSplashImage} className="mx-auto mainSplashImage" alt="main splash" />
+                </Col>
+            </Row>
+        </Container>
+    );
+};
 export default HomePage;
